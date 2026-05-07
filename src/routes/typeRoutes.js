eeef9d50a742cb40665ed3e860fd50141da9b0ee
TalitaFavore <talitafavore@gmail.com>
2026-05-07 15:23:55 -0300

    feat: adicionar rota do método create com post em typeRoutes.js

--- src/routes/typeRoutes.js
+++ src/routes/typeRoutes.js
@@ -4,6 +4,7 @@ import typeController from '../controller/typeController.js';
 const router = express.Router();
 
 router.get('/', typeController.getAll);
+router.post('/', typeController.create);
 
 export default router;
 
