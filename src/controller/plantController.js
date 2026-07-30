6ae5a65389e21b3de753d4b82c0be49674322ec5
TalitaFavore <talitafavore@gmail.com>
2026-07-30 16:29:06 -0300

    feat: adicionar métodos getAll, create e remove para plantas

---
+++ src/controller/plantController.js
@@ -0,0 +1,37 @@
+import plantModel from "../model/plantModel.js";
+
+const getAll = async (req, res) => {
+  const plants = await plantModel.getAll();
+  res.json(plants);
+};
+
+const create = async (req, res) => {
+  const { nome, preco, quantidade, id_tipos } = req.body;
+  if (!nome || !preco || !quantidade || !id_tipos) {
+    return res.status(400).json({
+      message: "Todos os campos são obrigatórios",
+    });
+  }
+  const newPlant = await plantModel.create(nome, preco, quantidade, id_tipos);
+  res.status(201).json(newPlant);
+};
+
+const remove = async (req, res) => {
+  const deletedPlant = await plantModel.remove(req.params.id);
+
+  if (!deletedPlant) {
+    return res.status(404).json({
+      message: "Planta não encontrado",
+    });
+  }
+  res.status(200).json({
+    message: "Planta deletada com sucesso",
+    deletedPlant,
+  });
+};
+
+export default {
+  getAll,
+  create,
+  remove,
+};
